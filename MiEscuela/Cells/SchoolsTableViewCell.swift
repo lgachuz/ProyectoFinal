52311a387a3aeddb319c6e722edfef306e299e70
lgachuz <secmisterio29@hotmail.com>
2020-02-16 18:15:04 -0600

    initial Commit

---
+++ MiEscuela/Cells/SchoolsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SchoolsTableViewCell.swift
+//  MiEscuela
+//
+//  Created by Luis Alberto Gachuz Morales on 16/02/20.
+//  Copyright © 2020 Luis Alberto Gachuz Morales. All rights reserved.
+//
+
+import UIKit
+
+class SchoolsTableViewCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MiEscuela/Models/School.swift
@@ -0,0 +1,16 @@
+//
+//  School.swift
+//  MiEscuela
+//
+//  Created by Luis Alberto Gachuz Morales on 16/02/20.
+//  Copyright © 2020 Luis Alberto Gachuz Morales. All rights reserved.
+//
+
+import Foundation
+
+struct School {
+    var name : String
+    var description : String
+    var ad_client_id : String
+    var acuerdoSEP : String
+}
