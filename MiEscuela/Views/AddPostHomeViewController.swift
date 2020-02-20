c2112f81029760ca69abd13456b032909530815d
lgachuz <secmisterio29@hotmail.com>
2020-02-20 00:29:43 -0600

    Add Tab Bar Controller and four sections

---
+++ MiEscuela/Views/AddPostHomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddPostHomeViewController.swift
+//  MiEscuela
+//
+//  Created by Luis Alberto Gachuz Morales on 19/02/20.
+//  Copyright © 2020 Luis Alberto Gachuz Morales. All rights reserved.
+//
+
+import UIKit
+
+class AddPostHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
